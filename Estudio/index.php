2230d83f74aef568a2fdf09d8f95c57d6af114c3
lxlalvarolxl <jesus-0199@hotmail.com>
2016-10-31 15:05:08 -0500

    Prueba

---
+++ Estudio/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>DavidAprende</title>
+	<link rel="stylesheet"  href="main.css">
+
+</head>
+
+
+<body>
+<?php
+
+
+
+?>
+<p id="<?php echo 'rojo'; ?>"> Esto es PHP </p>
+
+
+</body>
+
+</html>
